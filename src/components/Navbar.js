b76cbbf4bf2e2c3a4a08cda767811503a08bfa7f
JUSTGARCIA5 <JUSTGARCIA5@GMAIL.COM>
2021-12-20 16:15:11 -0800

    added font awesome icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faEquals } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
     return(
         <div className='nav'>
-            <i className='menu-icon'>=</i>
+            <i className='menu-icon'>
+                <FontAwesomeIcon icon={faEquals} />
+            </i>
             <a href='/' className='home-link'>
                 <b>
                     PUBLIC
@@ -15,9 +18,7 @@ export default function Navbar() {
                 </b>
             </a>
             <i className='search-icon'>
-                <FontAwesomeIcon
-                    icon={faSearch}
-                />
+                <FontAwesomeIcon icon={faSearch} />
             </i>
             <a href='/' className='cart-link'>Cart <b>0</b></a>
         </div>
